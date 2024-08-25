f10180346d4e4709f1592b6014d1b4c5c021ca9c
Nikolina Stojčić <stojcicn00@gmail.com>
2024-08-26 01:51:16 +0200

    Endpoint za dohvatanje profesora koji predaju nekom razredu i endpoint za dodavanje nove veze izmedju razreda i profesora(teaching)

--- ednevnik-backend/src/main/java/com/example/ednevnikbackend/controllers/TeachingController.java
+++ ednevnik-backend/src/main/java/com/example/ednevnikbackend/controllers/TeachingController.java
@@ -1,2 +1,37 @@
-package com.example.ednevnikbackend.controllers;public class TeachingController {
+package com.example.ednevnikbackend.controllers;
+
+import com.example.ednevnikbackend.daos.TeachingDAO;
+import com.example.ednevnikbackend.dtos.AddTeachingDTO;
+import com.example.ednevnikbackend.dtos.ProfessorDTO;
+import com.example.ednevnikbackend.dtos.TeachingDTO;
+import com.example.ednevnikbackend.services.TeachingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/teachings")
+public class TeachingController {
+
+    @Autowired
+    private TeachingService teachingService;
+//    @GetMapping("/byClassIdAndProfId/{classId}/{profId}")
+//    public ResponseEntity<?> getTeachingByClassIdAndProfessorId(@PathVariable Integer classId, @PathVariable Integer profId){
+//        return null;
+//    }
+
+
+    @GetMapping("/professors/{id}")
+    List<ProfessorDTO> getAllProfsBySchoolClassId(@PathVariable Integer id){
+       return teachingService.getAllBySchoolClassId(id);
+    }
+
+    @PostMapping
+    ResponseEntity<?> addTeaching(@RequestBody AddTeachingDTO req){
+        return new ResponseEntity<>(teachingService.addTeaching(req), HttpStatus.OK);
+    }
+
 }
--- ednevnik-backend/src/main/java/com/example/ednevnikbackend/services/TeachingService.java
+++ ednevnik-backend/src/main/java/com/example/ednevnikbackend/services/TeachingService.java
@@ -1,2 +1,18 @@
-package com.example.ednevnikbackend.services;public interface TeachingService {
+package com.example.ednevnikbackend.services;
+
+import com.example.ednevnikbackend.dtos.AddTeachingDTO;
+import com.example.ednevnikbackend.dtos.ProfessorDTO;
+import com.example.ednevnikbackend.dtos.TeachingDTO;
+import com.example.ednevnikbackend.models.Teaching;
+
+import java.util.List;
+
+public interface TeachingService {
+    List<Teaching> getByClassIdProfId(Integer classId, Integer profId);
+
+    TeachingDTO addTeaching(AddTeachingDTO req);
+
+
+
+    List<ProfessorDTO> getAllBySchoolClassId(Integer schoolClassId);
 }
